e7c4ded7a2211e55dc8c82103829ff1ecaacd3dd
Aminur Rahman <dev.aminur@gmail.com>
2024-12-07 15:14:37 +0600

    update image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,4 @@
-import {
-  FaGithub,
-  FaAngleRight,
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-} from "react-icons/fa6";
+import { FaGithub, FaAngleRight, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa6";
 
 const About = () => {
   return (
@@ -22,33 +16,25 @@ const About = () => {
               alt="2151005751"
             />
             <div className="-mt-16 mx-auto md:absolute top-10 -right-6 w-48 md:w-28 lg:w-40 border-8 border-yellow-20 rounded-full overflow-hidden z-10">
-              <img
-                className="scale-[1.6]"
-                src="https://i.ibb.co/2S7yddN/Blue-Modern-Linked-In-Profile-Picture.png"
-                alt=""
-              />
+              <img className="scale-[1.1]" src="./public/aminur.jpg" alt="" />
             </div>
           </div>
           <div className=" text-center md:text-left w-full md:w-3/5 lg:w-1/2 md:mt-6">
-            <h3 className="text-yellow-20 tracking-[5px] mb-3">
-              WELCOME TO MY WEBSITE
-            </h3>
+            <h3 className="text-yellow-20 tracking-[5px] mb-3">WELCOME TO MY WEBSITE</h3>
             <h2 className="text-3xl text-white md:text-4xl lg:text-5xl font-semibold mb-2">
               A LITTLE BIT ABOUT ME
             </h2>
             <div className="w-20 mx-auto md:mx-0 h-1.5 bg-yellow-20 my-5"></div>
             <div className="text-white mb-8">
               <p>
-                I specialize in front-end and MERN Stack web development. My
-                expertise in coding, troubleshooting intricate designs, and
-                ensuring project completion within deadlines.
+                I specialize in front-end and MERN Stack web development. My expertise in coding,
+                troubleshooting intricate designs, and ensuring project completion within deadlines.
               </p>
 
               <p>
-                I am dedicated to leveraging intelligent ideas through
-                innovative, standards-based, and open-source front-end and MERN
-                Stack development. Always eager for new challenges, I actively
-                seek opportunities to forge meaningful connections in the
+                I am dedicated to leveraging intelligent ideas through innovative, standards-based,
+                and open-source front-end and MERN Stack development. Always eager for new
+                challenges, I actively seek opportunities to forge meaningful connections in the
                 ever-evolving tech landscape.
               </p>
             </div>
--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const Banner = () => {
   return (
-    <div className="bg-[url(https://i.ibb.co/DYjLg47/2150898496.jpg)] bg-right-top ">
+    <div className="bg-[url(https://i.ibb.co/DYjLg47/2150898496.jpg)] bg-right-top bg-black">
       <div className="bg-[url(https://i.ibb.co/5jTpSGG/home-background-overlay.png)] bg-cover bg-no-repeat">
         <div className="bg-gradient-to-t from-black pt-36 pb-28">
           <div className="max-w-7xl mx-auto my-10 px-5 lg:px-0">
@@ -27,9 +27,9 @@ const Banner = () => {
               </span>
             </h1>
             <p className="text-base md:text-lg text-gray-100  max-w-2xl mb-10">
-              {"I'm"} a passionate Front End Developer with a love for creating
-              seamless user experiences. I specialize in React and enjoy turning
-              complex problems into elegant, intuitive solutions.
+              {"I'm"} a passionate Front End Developer with a love for creating seamless user
+              experiences. I specialize in React and enjoy turning complex problems into elegant,
+              intuitive solutions.
             </p>
             <a href="#projects">
               <button className="py-3 px-8 md:px-10 text-blue-1 bg-yellow-20 mr-5 rounded">
